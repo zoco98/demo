dd5279eb8bc66d10c97ba6498a4b6b672b5c46ad
Zocomon <sriparna1998@gmail.com>
2024-09-13 17:15:36 +0530

    commit to get all user API

---
+++ src/main/java/com/example/demo/controller/DemoController.java
@@ -0,0 +1,28 @@
+package com.example.demo.controller;
+
+
+import com.example.demo.entity.User;
+import com.example.demo.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/demo")
+public class DemoController {
+
+    @Autowired
+    private UserService userService;
+
+    /*
+     * curl --location 'http://localhost:8080/demo/getLoading'
+     */
+    @GetMapping("/getLoading")
+    public List<User> loadData(){
+        List<User> userList = userService.getAllUser();
+        return userList;
+    }
+}
--- src/main/java/com/example/demo/entity/User.java
+++ src/main/java/com/example/demo/entity/User.java
@@ -1,16 +1,26 @@
 package com.example.demo.entity;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.Getter;
-import lombok.Setter;
+import jakarta.validation.constraints.Pattern;
+import lombok.*;
 
-import javax.persistence.Table;
-
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+@Entity
 @Data
-@Table(name = "User")
+@Table(name = "user")
 @AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
 public class User {
+
+    @Id
+    @Column(name = "userId")
+    public String userId;
+    @Column(name = "userName")
+    public String userName;
+    @Column(name = "userEmailId")
+    public String userEmailId;
 }
